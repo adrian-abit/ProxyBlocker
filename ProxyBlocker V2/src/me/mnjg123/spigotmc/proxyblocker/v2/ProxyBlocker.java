a428571f0acb9bc71a2a806862be46426edbbf83
mnjg123 <mnjg123@mnjg123>
2020-05-28 11:23:45 +0200

    First Commit

---
+++ ProxyBlocker V2/src/me/mnjg123/spigotmc/proxyblocker/v2/ProxyBlocker.java
@@ -0,0 +1,20 @@
+package me.mnjg123.spigotmc.proxyblocker.v2;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ProxyBlocker extends Plugin{
+	
+	@Override
+	public void onEnable() {
+		
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		super.onDisable();
+	}
+	
+
+}
