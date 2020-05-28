a3a22fd1d423e45571f38f0d444218d2e0aa44d3
mnjg123 <mnjg123@mnjg123>
2020-05-28 22:05:00 +0200

    Updates in Comments

--- ProxyBlocker V2/src/me/mnjg123/spigotmc/proxyblocker/v2/ProxyBlocker.java
+++ ProxyBlocker V2/src/me/mnjg123/spigotmc/proxyblocker/v2/ProxyBlocker.java
@@ -1,20 +1,71 @@
+/**                                                                                                                                              
+ *                   @@@@@@@@@@                       @@@@@@@@@@                  
+ *                  @@@@@@@@@@@                       @@@@@@@@@@@                 
+ *                  @@@@@@                                 @@@@@@                 
+ *                  @@@@@@   @@@@@@@@@@@@@@@@@@@@@@        @@@@@@                 
+ *                  @@@@@@   @@@@@@@@@@@@@@@@@@@@@@@@@@    @@@@@@                 
+ *                  @@@@@@   @@@@@@@@@@@@@@@@@@@@@@@@@@@   @@@@@@                 
+ *                @@@@@@@    @@@@@@@              @@@@@@@   @@@@@@@                
+ *              @@@@@@@      @@@@@@@    @@@@@@    @@@@@@@     @@@@@@@             
+ *              @@@@@@@      @@@@@@@    @@@@@@    @@@@@@@     @@@@@@@             
+ *                @@@@@@@    @@@@@@@    @@@@@@    @@@@@@@   @@@@@@@               
+ *                  @@@@@@   @@@@@@@    @@@@@@    @@@@@@@  @@@@@@                 
+ *                  @@@@@@   @@@@@@@    @@@@@@    @@@@@@@  @@@@@@                 
+ *                  @@@@@@   @@@@@@@    @@@@@@    @@@@@@@  @@@@@@                 
+ *                  @@@@@@   @@@@@@@    @@@@@@    @@@@@@@  @@@@@@                 
+ *                  @@@@@@                                 @@@@@@                 
+ *                  @@@@@@@@@@@                       @@@@@@@@@@@                 
+ *                   @@@@@@@@@@                       @@@@@@@@@@                  
+ *                                                                                
+ *                                                                                
+ *                               @@@                @@@@@      @@@@@@@    @@@@@@@ 
+ *                                                    @@@     @@@   @@@  @@@   @@@
+ * @@@@@@@@@@@@@@@   @@@@@@@@@   @@@  @@@@@@@@@@@     @@@           @@@     @@@@@ 
+ * @@    @@@    @@@  @@@    @@   @@@  @@      @@@     @@@         @@@          @@@
+ * @@    @@@    @@@  @@@    @@   @@@  @@@    @@@@     @@@      @@@@      @@@   @@@
+ * @@    @@@    @@@  @@@    @@   @@@    @@@@@@@@@     @@@    @@@@@@@@@@   @@@@@@@ 
+ *                              @@@   @@@@@@@@@@                                  
+ */
+
+
+/*******************************************************************************
+ * @author mnjg123
+ * 
+ * @website https://mnjg123.dev/
+ *
+ * Copyright (c) 2020 - present mnjg123.
+ * All rights reserved. 
+ *
+ * @license
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ *   mnjg123 
+ *******************************************************************************/
+
 package me.mnjg123.spigotmc.proxyblocker.v2;
 
 import net.md_5.bungee.api.plugin.Plugin;
 
-public class ProxyBlocker extends Plugin{
-	
+/**
+ * @author mnjg123
+ *
+ */
+public class ProxyBlocker extends Plugin {
+
 	@Override
 	public void onEnable() {
 		
 		super.onEnable();
 	}
-	
+
 	@Override
 	public void onDisable() {
-		
+	
 		super.onDisable();
 	}
 	
-
 }
